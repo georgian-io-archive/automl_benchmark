2bd8395698b83501de6700c195f3276bde92f246
Alexander Allen <admin@altechcode.com>
2018-06-12 11:29:53 -0400

    Tweak memory requirements for batch

--- benchmark.py
+++ benchmark.py
@@ -10,8 +10,8 @@ from sklearn import metrics
 from tqdm import tqdm
 
 TIME_PER_TASK = 10800 # seconds (3 hours)
-MIN_MEM = '3700M'
-MAX_MEM = '3700M'
+MIN_MEM = '3490M'
+MAX_MEM = '3490M'
 N_CORES = 2
 
 def process_auto_sklearn(X_train, X_test, y_train, df_types, m_type, seed):
