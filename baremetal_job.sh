9c651860ec2d7360dc859f2badf2c9dac8d20d8d
Alexander Allen <admin@altechcode.com>
2018-06-18 16:12:33 -0400

    Fix job cleanup for baremetal

--- baremetal_job.sh
+++ baremetal_job.sh
@@ -7,6 +7,9 @@ source automl_benchmark/bin/activate
 
 timeout 12600 python baremetal_wrapper.py
 
+killall java
+killall python
+
 aws s3 cp /home/ec2-user/logs.out s3://${S3_BUCKET}/${S3_FOLDER}h2o_logs/${TASK}/$(date +%Y%m%d%H%M%S).log
 
 cp /home/ec2-user/logs.out /home/ec2-user/${TASK}.log
